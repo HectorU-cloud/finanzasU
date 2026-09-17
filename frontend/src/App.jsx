32363cea0270e4d378b560853c52f254aa3a24e0
HectorU-cloud <jmbf5rb5g9@privaterelay.appleid.com>
2026-09-17 16:01:40 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -337,7 +337,6 @@ const [tokenReset] = useState(() => leerTokenResetDeUrl());
             setOrigenDetalle("tarjetas");
             setTarjetaDetalle(t);
           }}
-          }}
         />
 
         {modalTarjetas && (
